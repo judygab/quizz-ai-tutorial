02265395387a68f1cf08ecf359ab1e50350f0c07
judygab <ivditigabeskiria@gmail.com>
2024-04-09 11:59:56 -0400

    update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1">
       <main className="flex justify-center flex-1">
-        <h1 className="text-6xl font-bold">Welcome to the quizz page👋</h1>
-    </main>
-      <footer className="footer pb-9 px-6 relative mb-0">
-        <Button>Start</Button>
-      </footer>
+        <div className="items-center flex flex-col sm:flex-row gap-20 justify-end mx-auto p-10 w-full sm:py-20 sm:w-[1000px]">
+          <div>
+            <Image src="/images/owl-landing-no-bg.png" width="400" height="400" alt="owl" />
+          </div>
+          <div className="text-center flex gap-6 flex-col">
+            <h1 className="text-3xl font-bold">Get quizzed about anything!</h1>
+            <h3 className="text-sm">Upload documents, and easily generate your quizzes with AI.</h3>
+            <Button variant="neo" className="mt-4 h-14 text-white" asChild><Link href="quizz/new">Get Started</Link></Button>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
